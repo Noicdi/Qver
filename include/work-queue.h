4bfc3f20ac39a463c5b5c312346bae8058b97994
xQmQ <xqmq99@gmail.com>
2021-08-14 07:03:49 +0800

    代码：线程池的任务队列

---
+++ include/work-queue.h
@@ -0,0 +1,89 @@
+/*
+ * Author: xQmQ
+ * Date: 2021/8/14 6:45
+ * Description: 线程池中用来保存读写请求的任务队列，供工作线程使用
+ */
+
+#ifndef QVER_INCLUDE_WORK_QUEUE_H_
+#define QVER_INCLUDE_WORK_QUEUE_H_
+
+#include <mutex>
+#include <queue>
+
+template<typename T>
+class WorkQueue {
+private:
+  std::queue<T> work_queue_; // 任务队列
+  std::mutex mutex_;         // 互斥锁
+public:
+  WorkQueue() = default;
+
+  WorkQueue(const WorkQueue &) = delete;
+
+  WorkQueue(WorkQueue &&) = delete;
+
+  WorkQueue &operator=(const WorkQueue &) = delete;
+
+  WorkQueue &operator=(WorkQueue &&) = delete;
+
+  ~WorkQueue() = default;
+
+  bool empty() const;
+
+  int size() const;
+
+  /*
+   * description: 向队尾推入元素
+   * param: {t->被推入元素}
+   * return: {}
+   */
+  void pushQueue(const T &t);
+
+  /*
+   * description: 从队首摘出元素
+   * param: {t->用以保存被摘出元素}
+   * return: {true->摘出元素成功; false->队列为空}
+   */
+  bool fetchQueue(T &t);
+};
+
+template<typename T>
+bool
+WorkQueue<T>::empty() const
+{
+  std::unique_lock<std::mutex> lock(mutex_);
+
+  return work_queue_.empty();
+}
+
+template<typename T>
+int
+WorkQueue<T>::size() const
+{
+  std::unique_lock<std::mutex> lock(mutex_);
+
+  return work_queue_.size();
+}
+
+template<typename T>
+void
+WorkQueue<T>::pushQueue(const T &t)
+{
+  std::unique_lock<std::mutex> lock(mutex_);
+  work_queue_.template emplace(t);
+}
+
+template<typename T>
+bool
+WorkQueue<T>::fetchQueue(T &t)
+{
+  std::unique_lock<std::mutex> lock(mutex_);
+  if (work_queue_.empty())return false;
+
+  t = std::move(work_queue_.front());
+  work_queue_.pop();
+
+  return true;
+}
+
+#endif //QVER_INCLUDE_WORK_QUEUE_H_
