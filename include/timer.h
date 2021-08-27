16623668eaf6077a26768db7d41e8adddea567e1
xQmQ <xqmq99@gmail.com>
2021-08-27 19:55:39 +0800

    模块：定时器链表，用以处理非活动链接

---
+++ include/timer.h
@@ -0,0 +1,58 @@
+/*
+ * Author: xQmQ
+ * Date: 2021/8/24 19:37
+ * Description: 处理非活动连接的定时器，对超时的非活动连接做关闭操作
+ */
+
+#ifndef QVER_INCLUDE_TIMER_H_
+#define QVER_INCLUDE_TIMER_H_
+
+#include "../include/non-copyable.h"
+
+#include <time.h>
+#include <vector>
+
+class Timer : NonCopyable {
+public:
+  explicit Timer():head_ptr_(nullptr), tail_ptr_(nullptr){}
+
+  ~Timer();
+
+  /*
+   * description: 向定时容器中添加待监控的 socket
+   * param: {fd->待监控的 socket}
+   * return: {}
+   */
+  void push(int fd);
+
+  /*
+   * description: 删除特定的 socket
+   * param: {fd->被删除的fd}
+   * return: {-1->删除失败; 0->删除成功}
+   */
+  int pop(int fd);
+
+  /*
+   * description: 收集到期文件描述符集，由其他方法做处理
+   * param: {timeout_fds->到期文件描述符集}
+   * return: {}
+   */
+  void tick(std::vector<int> &timeout_fds);
+
+private:
+  class UtilTimer {
+  public:
+    explicit UtilTimer(int fd, int timeout = 10);
+
+  public:
+    int fd_;                  // 被监控的文件描述符
+    time_t expiration_time_; // 超时时间，绝对时间
+    UtilTimer *prev_ptr_;    // 前一个计时器
+    UtilTimer *next_ptr_;    // 后一个计时器
+  };
+
+  UtilTimer *head_ptr_; // 头结点
+  UtilTimer *tail_ptr_; // 尾结点
+};
+
+#endif //QVER_INCLUDE_TIMER_H_
---
+++ src/timer.cpp
@@ -0,0 +1,95 @@
+/*
+ * Author: xQmQ
+ * Date: 2021/8/24 19:37
+ * Description: 
+ */
+
+#include "../include/timer.h"
+
+Timer::UtilTimer::UtilTimer(int fd, int timeout) : fd_(fd), prev_ptr_(nullptr), next_ptr_(nullptr)
+{
+  expiration_time_ = time(nullptr) + timeout;
+}
+
+Timer::~Timer()
+{
+  UtilTimer *temp_ptr = head_ptr_;
+  while (temp_ptr) {
+    head_ptr_ = temp_ptr->next_ptr_;
+    delete temp_ptr;
+    temp_ptr = head_ptr_;
+  }
+}
+
+void
+Timer::push(int fd)
+{
+  if (fd < 0)
+    return;
+
+  UtilTimer *temp_ptr = new UtilTimer(fd);
+
+  if (!head_ptr_) {
+    head_ptr_ = tail_ptr_ = temp_ptr;
+    return;
+  }
+
+  temp_ptr->prev_ptr_ = tail_ptr_;
+  tail_ptr_->next_ptr_ = temp_ptr;
+  tail_ptr_ = temp_ptr;
+}
+
+int
+Timer::pop(int fd)
+{
+  if (fd < 0 || !head_ptr_)
+    return -1;
+
+  UtilTimer *temp_ptr = head_ptr_;
+  while (temp_ptr) {
+    if (temp_ptr->fd_ == fd)
+      break;
+    temp_ptr = temp_ptr->next_ptr_;
+  }
+
+  if (temp_ptr == head_ptr_ && temp_ptr == tail_ptr_) {
+    delete temp_ptr;
+    head_ptr_ = tail_ptr_ = nullptr;
+  } else if (temp_ptr == head_ptr_) {
+    head_ptr_ = head_ptr_->next_ptr_;
+    head_ptr_->prev_ptr_ = nullptr;
+    delete temp_ptr;
+  } else if (temp_ptr == tail_ptr_) {
+    tail_ptr_ = tail_ptr_->prev_ptr_;
+    tail_ptr_->next_ptr_ = nullptr;
+    delete temp_ptr;
+  } else {
+    temp_ptr->prev_ptr_->next_ptr_ = temp_ptr->next_ptr_;
+    temp_ptr->prev_ptr_->next_ptr_ = temp_ptr->prev_ptr_;
+    delete temp_ptr;
+  }
+  return 0;
+}
+
+void
+Timer::tick(std::vector<int> &timeout_fds)
+{
+  timeout_fds.clear();
+
+  if (head_ptr_) {
+    time_t cur_time = time(nullptr);
+    UtilTimer *temp_ptr = head_ptr_;
+    while (temp_ptr) {
+      if (cur_time < temp_ptr->expiration_time_)
+        break;
+      timeout_fds.push_back(temp_ptr->fd_);
+      pop(temp_ptr->fd_);
+      temp_ptr = head_ptr_;
+    }
+  }
+}
+
+
+
+
+
